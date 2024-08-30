f15889c72b4963d3e90583b3481edf7a3257ad52
magnus012 <igor_012@hotmail.com>
2024-08-30 10:04:00 -0300

    desenvolvendo componentes de layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Pagina from "./components/layouts/Pagina";
 function App() {
   return (
     <div className="App">
-      
+      <Pagina>
+        <h1>Esta é a pagina de cadastro de cliente</h1>
+      </Pagina>
+      <Pagina>
+        <h1>Esta é a pagina de cadastro de produto</h1>
+      </Pagina>
     </div>
   );
 }
